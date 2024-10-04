8b9bfda15237d1a825cbdbfb52e26b9c32cd0b08
unknown <yuniordavalos@outlook.es>
2024-10-04 14:42:03 -0400

    Primer Commit

---
+++ mvcProyect/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvcProyect.Models;
+namespace mvcProyect.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+         : base(options)
+        {
+        }
+        //Aqui todos los modelos que se creen 
+        public DbSet<Producto> Producto { get; set; }
+
+    }
+}
